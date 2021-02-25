21a773f7cf4466e4017167669970ba52cb49d258
Pietro Piraino <curciopietro91@gmail.comgit config --global user.email curciopietro91@gmail.com>
2021-02-25 16:19:11 +0100

    piccola modifica

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import "./App.css";
 import Header from "./components/header";
 import Body from "./components/body";
 import Footer from "./components/footer";
+import data from "./components/data.js";
 
 function App() {
-  const data = {
-    name: "HappyShoes",
-    logo: "https://logoipsum.com/logo/logo-16.svg",
-    company: "SZH Inc.",
-    cover:
-      "https://images.pexels.com/photos/4123897/pexels-photo-4123897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    title: "Welcome to our brand new HappyShoes website!",
-    description:
-      "Lorem ipsum velit cillum excepteur adipisicing voluptate mollit. Laboris deserunt reprehenderit commodo id id minim non voluptate irure pariatur nulla.",
-  };
-
   return (
     <div className="App">
       <Header url={data.logo} webSiteName={data.name} />
---
+++ src/components/data.js
@@ -0,0 +1,12 @@
+const data = {
+  name: "HappyShoes",
+  logo: "https://logoipsum.com/logo/logo-16.svg",
+  company: "SZH Inc.",
+  cover:
+    "https://images.pexels.com/photos/4123897/pexels-photo-4123897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  title: "Welcome to our brand new HappyShoes website!",
+  description:
+    "Lorem ipsum velit cillum excepteur adipisicing voluptate mollit. Laboris deserunt reprehenderit commodo id id minim non voluptate irure pariatur nulla.",
+};
+
+export default data;
